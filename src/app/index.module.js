3c1c3aaa27e467437e0d8c8a1e7f91ae95ecf3a0
Viacheslav Muzyka <Viacheslav Muzyka>
2017-03-11 18:07:10 +0200

    make all except the maps

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,19 +5,16 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
+
+let API_URL = 'http://212.80.38.228:3333/';
 
 angular.module('ecologyMonitoring', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr'])
   .constant('malarkey', malarkey)
   .constant('moment', moment)
+  .constant('API_URL', API_URL)
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
-  .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
+  .directive('acmeNavbar', NavbarDirective);
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,57 +1,120 @@
 export class MainController {
-    constructor($timeout, webDevTec, toastr, $http) {
-        'ngInject';
+  constructor($http, API_URL) {
+    'ngInject';
 
-        this.awesomeThings = [];
-        this.classAnimation = '';
-        this.creationDate = 1489177324090;
-        this.toastr = toastr;
-        this.$http = $http;
-        this.activate($timeout, webDevTec);
+    this.constantsProb = {
+      1: {
+        a: -9.15,
+        b: 11.66
+      },
+      2: {
+        a: -5.51,
+        b: 7.49
+      },
+      3: {
+        a: -2.35,
+        b: 3.73
+      },
+      4: {
+        a: -1.41,
+        b: 2.33
+      }
+    };
+
+    this.constantsRisk = {
+      1: {
+        k: 7.5,
+        b: 2.35
+      },
+      2: {
+        k: 6,
+        b: 1.28
+      },
+      3: {
+        k: 4.5,
+        b: 1
+      },
+      4: {
+        k: 3,
+        b: 0.87
+      }
+    };
+
+    this.$http = $http;
+    this.newSubstance = {};
+    this.newPollution = {};
+    this.currentTab = 'item';
+    this.API_URL = API_URL;
+    this.getData();
+    this.getAllSubstances();
+  }
+
+  getData() {
+    this.$http
+      .get(`${this.API_URL}pollutions`)
+      .then(response => {
+        this.pollutions = response.data;
+      })
+  }
+
+  getAllSubstances() {
+    this.$http
+      .get(`${this.API_URL}substances`)
+      .then(response => {
+        this.substances = response.data;
+      })
+  }
+
+  getProb(item) {
+    return this.constantsProb[item.classOfDangerous].a + (this.constantsProb[item.classOfDangerous].b * Math.log10(item.averageConcentration / item.gdk));
+  }
+
+  getRisk(item) {
+    return 1 - Math.exp((Math.log(0.84) * (item.averageConcentration / item.gdk) * (this.constantsRisk[item.classOfDangerous].k / this.constantsRisk[item.classOfDangerous].b)));
+  }
+
+  changeTab(tab) {
+    this.currentTab = tab;
+  }
+
+  checkDigitsAvg(that) {
+    if (that.main.newPollution.avg) {
+      that.main.newPollution.avg = that.main.newPollution.avg.replace(/[A-Za-zа-яА-ЯёЁ!,-]/, "");
+    }
+  }
+
+  checkDigitsGDK(that) {
+    if (that.main.newSubstance.gdk) {
+      that.main.newSubstance.gdk = that.main.newSubstance.gdk.replace(/[A-Za-zа-яА-ЯёЁ!,-]/, "");
+    }
+  }
+
+  addSubstance(substance) {
+    this.$http
+      .post(`${this.API_URL}substances`, {
+        name: substance.name,
+        gdk: substance.gdk,
+        classOfDangerous: substance.classDanger
+      })
+      .then(response => {
+        $("#addItemModal").modal('hide');
+        this.substances.push(response.data);
         this.newSubstance = {};
-        this.testNode();
-    }
+      });
+  }
 
-    testNode() {
-        this.$http
-            .get(`http://localhost:9999/substances`)
-            .then(response => {
-                this.substances = response.data;
-                console.log(response.data);
-            })
-    }
-
-    addItem(substance) {
-        this.$http
-            .post(`http://localhost:9999/substances`, {
-                name: substance.name,
-                city: substance.city,
-                averageConcentration: substance.avg,
-                gdk: substance.gdk,
-                classOfDangerous: substance.classDanger
-            })
-            .then(response => {
-                $("#addItemModal").modal('hide');
-            });
-    }
-
-    activate($timeout, webDevTec) {
-        this.getWebDevTec(webDevTec);
-        $timeout(() => {
-            this.classAnimation = 'rubberBand';
-        }, 4000);
-    }
-
-    getWebDevTec(webDevTec) {
-        this.awesomeThings = webDevTec.getTec();
-
-        angular.forEach(this.awesomeThings, (awesomeThing) => {
-            awesomeThing.rank = Math.random();
-        });
-    }
-
-    showToastr() {
-        this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-        this.classAnimation = '';
-    }
+  addItem(pollution) {
+    this.$http
+      .post(`${this.API_URL}pollutions`, {
+        city: pollution.city,
+        averageConcentration: pollution.avg,
+        mainLocation: pollution.mainLocation,
+        substanceId: pollution.substanceId
+      })
+      .then(response => {
+        $("#addItemModal").modal('hide');
+        this.newPollution = {};
+        this.pollutions.push(response.data);
+      });
+  }
 }
